058e2f5302ec34d8020c12ec72824e7e7659965b
snail <snailpan@foxmail.com>
2017-11-09 16:06:26 +0800

    clean

--- demo-rest/src/main/java/com/snail/HelloController.java
+++ demo-rest/src/main/java/com/snail/HelloController.java
@@ -4,16 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
-   *  Created by snail  2017年9月7日
-   *  
-   */
+ * Created by snail 2017年9月7日
+ * 
+ */
 
 @RestController
 @RequestMapping("/index")
 public class HelloController {
-	
+
 	@RequestMapping("/hello")
-	private String hello(){
+	private String hello() {
 		return "Hello SpringBoot";
 	}
 
