0883000c95af44d0b08f16aaca7b8ecb42f8ad95
Qsnailpan <snailpan@foxmail.com>
2018-03-20 10:30:44 +0800

    完善 key 存入redis 时乱码问题

---
+++ demo-redis/src/main/java/com/snail/config/RedisCon.java
@@ -0,0 +1,50 @@
+package com.snail.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ *  
+ * @Description: redisTemplate 序列化使用的jdkSerializeable, 存储二进制字节码, 所以自定义序列化类
+ * 
+ * 存储 key : city_1
+ * 修改之前redis中真实存储 key 示例 ： \xAC\xED\x00\x05t\x00\x06city_1  
+ * 修改之后存储 key 示例 ： city_1  
+ * 
+ * @author lipan
+ * @date 2018年3月19日
+
+ */
+@Configuration
+public class RedisCon {
+
+	@Bean
+	public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+		// 使用Jackson2JsonRedisSerialize 替换默认序列化
+		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+		// 设置value的序列化规则和 key的序列化规则
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
+	}
+
+}
